de62315b0fdc982a06c550736c79ac16be0fb039
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-03-10 21:06:40 +0100

    Final fix to env installation script

--- install-pointcept.sh
+++ install-pointcept.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "Starting installation..."
 git clone https://github.com/ES-25-CE-8-846/ucloud.git
 
-
+echo "Creating installation script..."
 cat ucloud/install-scripts/cuda-toolkit-118.sh >> install-script.sh
 echo "
 " >> install-script.sh
@@ -15,7 +16,10 @@ cat ucloud/install-scripts/pointcept-cu118.sh >> install-script.sh
 echo "
 " >> install-script.sh
 
+echo "Running install-script.sh..."
 sh install-script.sh
 rm install-script.sh
+
+echo "conda activate pointcept" >> $HOME/.bashrc
 
 echo "Installation completed successfully."
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/ES-25-CE-8-846/ucloud.git
-
-sh ucloud/install-scripts/cuda-toolkit-118.sh
-sh ucloud/install-scripts/miniconda.sh
-sh ucloud/install-scripts/pointcept-cu118.sh
-
-echo "Installation completed successfully."
