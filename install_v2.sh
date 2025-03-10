317d9845bea3488d668502aefddab4f2c9cc4a74
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-03-10 20:53:25 +0100

    tryin summin fånni

---
+++ install_v2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/ES-25-CE-8-846/ucloud.git
+
+
+cat ucloud/install-scripts/cuda-toolkit-118.sh >> install-script.sh
+cat ucloud/install-scripts/miniconda.sh >> install-script.sh
+cat ucloud/install-scripts/pointcept-cu118.sh >> install-script.sh
+sh install-script.sh
+rm install-script.sh
+
+echo "Installation completed successfully."
